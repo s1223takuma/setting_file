a842dbc81bdd733571cdbaa4f371529460734b62
soda <114852162+s1223takuma@users.noreply.github.com>
2025-12-14 07:58:04 +0900

    setting_file各種

---
+++ nvim_cp/lua/keymaps.lua
@@ -0,0 +1,101 @@
+local opts = { noremap = true, silent = true }
+local term_opts = { silent = true }
+
+--local keymap = vim.keymap
+local keymap = vim.api.nvim_set_keymap
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Modes
+--   normal_mode = 'n',
+--   insert_mode = 'i',
+--   visual_mode = 'v',
+--   visual_block_mode = 'x',
+--   term_mode = 't',
+--   command_mode = 'c',
+
+-- Brtter window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- New tab
+keymap("n", "te", ":tabedit", opts)
+
+-- 新しいタブを一番右に作る
+keymap("n", "gn", ":tabnew<Return>", opts)
+
+-- move tab
+keymap("n", "gh", "gT", opts)
+keymap("n", "gl", "gt", opts)
+
+-- ファイルの一括移動
+keymap("n", "<leader>h", "0", opts)
+keymap("n", "<leader>j", "G", opts)
+keymap("n", "<leader>k", "gg", opts)
+keymap("n", "<leader>l", "$", opts)
+
+-- Neotreeとの移動キーを<Space>+fに変更
+keymap("n", "<leader>f", "<Cmd>wincmd h<CR>", opts)
+
+-- Split window
+keymap("n", "ss", ":split<Return><C-w>w", opts)
+keymap("n", "sv", ":vsplit<Return><C-w>w", opts)
+
+-- Select all
+keymap("n", "<C-a>", "gg<S-v>G", opts)
+
+-- Do not yank with x
+keymap("n", "x", '"_x', opts)
+
+-- Delete a word backwards
+keymap("n", "dw", 'vb"_d', opts)
+
+keymap("n", "<Space>l", "$", opts)
+
+-- ;でコマンド入力( ;と:を入れ替)
+keymap("n", ";", ":", opts)
+
+-- 行末までのヤンクにする
+keymap("n", "Y", "y$", opts)
+
+-- <Space>q で強制終了
+keymap("n", "<Space>q", ":<C-u>q!<Return>", opts)
+
+-- ESC*2 でハイライトやめる
+keymap("n", "<Esc><Esc>", ":<C-u>set nohlsearch<Return>", opts)
+
+-- Insert --
+-- Press jk fast to exit insert mode
+keymap("i", "jk", "<ESC>", opts)
+
+-- コンマの後に自動的にスペースを挿入
+keymap("i", ",", ",<Space>", opts)
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- ビジュアルモード時vで行末まで選択
+keymap("v", "v", "$h", opts)
+
+-- 0番レジスタを使いやすくした
+keymap("v", "<C-p>", '"0p', opts)
+
+-- Neotreeを使いやすくした
+keymap("n", "<leader>e", ":Neotree toggle<Return>", opts)
+
+-- 全選択
+keymap("n", "<leader>a", "ggVG", opts)
+
+-- 矢印キーを反応しなくする
+vim.keymap.set("n", "<Up>", "5k")
+vim.keymap.set("n", "<Down>", "5j")
+vim.keymap.set("", "<Left>", "<Nop>")
+vim.keymap.set("", "<Right>", "<Nop>")
+
